bb79fbc267e1e0149bb682bb32305ebb71ab42fa
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2025-06-25 19:00:11 -0400

    Fix id conflicts + shader/graphics tweaks

--- packwiz-refresh.bat
+++ packwiz-refresh.bat
@@ -1,4 +1,6 @@
 SET dir=C:\GitHub\Thaumcraft4Pack\resourcepacks\
 jar -cfM %dir%Essential-Textures.zip -C %dir%Essential-Textures\ .
 
+SET dir2=C:\GitHub\Thaumcraft4Pack\shaderpacks\
+jar -cfM %dir2%Complementary-Shaders.zip -C %dir2%Complementary-Shaders\ .
 packwiz refresh
