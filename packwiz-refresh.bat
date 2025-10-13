48509df7dc3c3fd5e7bbdd01936cbd76d6af7606
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2025-10-13 00:39:38 -0400

    Mod Update + shader Update
    - New Aether, RPLE fix, and shader fix

--- packwiz-refresh.bat
+++ packwiz-refresh.bat
@@ -1,6 +1,3 @@
 SET dir=C:\GitHub\Thaumcraft4Pack\resourcepacks\
 jar -cfM %dir%Essential-Textures.zip -C %dir%Essential-Textures\ .
-
-SET dir2=C:\GitHub\Thaumcraft4Pack\shaderpacks\
-jar -cfM %dir2%Complementary-Shaders.zip -C %dir2%Complementary-Shaders\ .
 packwiz refresh
